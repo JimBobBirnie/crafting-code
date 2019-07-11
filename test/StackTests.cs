8aff46db89418a01dacc808f5b79869f15edf16f
James Birnie <jimbobbirnie@JamesBirniePersonal.local>
2019-07-11 12:32:39 +0100

    added Xunit and built the solution successfully
    JB

---
+++ test/StackTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+
+namespace CraftingCode
+{
+    public class StackTests
+    {
+        [Fact]
+        public void TestName()
+        {
+            //Given
+
+            //When
+
+            //Then
+            Assert.True(true);
+        }
+    }
+}
