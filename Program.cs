fb7abd282d5658cc1c406d0ba6274a6f65d9ce1c
James Birnie <jimbobbirnie@JamesBirniePersonal.local>
2019-07-11 10:46:42 +0100

    initial commit
    JB

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace crafting_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
