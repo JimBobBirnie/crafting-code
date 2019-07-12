3b837e8890b48b13ee4626c89c47abb3ba155650
James Birnie <jimbobbirnie@JamesBirniePersonal.local>
2019-07-12 10:52:39 +0100

    all the cases are covered by tests
    JB

---
+++ src/RomanNumberConverter.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+
+namespace CraftingCode
+{
+    public class RomanNumeralConverter
+    {
+        private static Dictionary<char, int> _characterValues = new Dictionary<char, int>{
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        public static int Convert(string romanNumeral)
+        {
+            var runningTotal = 0;
+            for (int i = 0; i < romanNumeral.Length; i++)
+            {
+                var currentCharacterValue = _characterValues[romanNumeral[i]];
+                bool isDeductionPair = false;
+                if (i < romanNumeral.Length - 1)
+                {
+                    var nextCharacterValue = _characterValues[romanNumeral[i + 1]];
+                    isDeductionPair = currentCharacterValue < nextCharacterValue;
+                    if (isDeductionPair)
+                    {
+                        runningTotal += (nextCharacterValue - currentCharacterValue);
+                        i++;
+                    }
+                    else
+                    {
+                        runningTotal += _characterValues[romanNumeral[i]];
+                    }
+                }
+                else
+                {
+                    runningTotal += _characterValues[romanNumeral[i]];
+                }
+            }
+            return runningTotal;
+        }
+
+        private static int GetCharacterEquivalent(char singleCharacter)
+        {
+            return _characterValues[singleCharacter];
+        }
+    }
+}
---
+++ test/RomanNumeralConverterTests.cs
@@ -0,0 +1,56 @@
+using System;
+using Xunit;
+
+namespace CraftingCode
+{
+    public class RomanNumeralConverterTests
+    {
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("V", 5)]
+        [InlineData("X", 10)]
+        [InlineData("L", 50)]
+        [InlineData("C", 100)]
+        [InlineData("D", 500)]
+        [InlineData("M", 1000)]
+        public void SingleCharacterTests(string romanNumeral, int decimalEquivalent)
+        {
+            var actual = RomanNumeralConverter.Convert(romanNumeral);
+            Assert.Equal(decimalEquivalent, actual);
+        }
+
+        [Theory]
+        [InlineData("II", 2)]
+        [InlineData("XX", 20)]
+        [InlineData("CC", 200)]
+        [InlineData("MM", 2000)]
+        [InlineData("MMC", 2100)]
+        public void MultipleCharacterTests(string romanNumeral, int decimalEquivalent)
+        {
+            var actual = RomanNumeralConverter.Convert(romanNumeral);
+            Assert.Equal(decimalEquivalent, actual);
+        }
+
+        [Theory]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("XL", 40)]
+        [InlineData("XC", 90)]
+        [InlineData("CM", 900)]
+        public void SmallerNumberDeductsFromSubsequentLargerNumber(string romanNumeral, int decimalEquivalent)
+        {
+            var actual = RomanNumeralConverter.Convert(romanNumeral);
+            Assert.Equal(decimalEquivalent, actual);
+        }
+
+        [Theory]
+        [InlineData("MMCDXCIX", 2499)]
+        [InlineData("MMMCMXLIX", 3949)]
+        public void TheComplexCases(string romanNumeral, int decimalEquivalent)
+        {
+            var actual = RomanNumeralConverter.Convert(romanNumeral);
+            Assert.Equal(decimalEquivalent, actual);
+        }
+    }
+
+}
