4a77780d70dc464bb2ea7de4ed94337d7d0bada6
James Birnie <jimbobbirnie@JamesBirniePersonal.local>
2019-07-16 09:52:56 +0100

    added cases to convert things that have a single letter equivalent
    JB

--- src/RomanNumberConverter.cs
+++ src/RomanNumberConverter.cs
@@ -15,6 +15,29 @@ namespace CraftingCode
             {'M', 1000}
         };
 
+        public static string ConvertToRomanNumerals(int decimalNumber)
+        {
+            switch (decimalNumber)
+            {
+                case 1:
+                    return "I";
+                case 5:
+                    return "V";
+                case 10:
+                    return "X";
+                case 50:
+                    return "L";
+                case 100:
+                    return "C";
+                case 500:
+                    return "D";
+                case 1000:
+                    return "M";
+                default:
+                    return null;
+            }
+        }
+
         private static Dictionary<char, List<char>> _allowedDeductionPairs = new Dictionary<char, List<char>>{
             {'I', new List<char>{'V','X'}},
             {'X', new List<char>{'L', 'C'}},
---
+++ test/ConvertToRomanNumeralTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace CraftingCode
+{
+    public class ConvertToRomanNumeralTests
+    {
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(5, "V")]
+        [InlineData(10, "X")]
+        [InlineData(50, "L")]
+        [InlineData(100, "C")]
+        [InlineData(500, "D")]
+        [InlineData(1000, "M")]
+        public void ShouldConvertSingleLetterEquivalents(int decimalNumber, string expectedRomanNueral)
+        {
+            var actual = RomanNumeralConverter.ConvertToRomanNumerals(decimalNumber);
+            Assert.Equal(expectedRomanNueral, actual);
+        }
+    }
+}
