9c412254e3e6286c0c14c7b8c70b9a4ae967798e
James Birnie <jimbobbirnie@JamesBirniePersonal.local>
2019-07-16 09:55:57 +0100

    added roman equivalent dictionary and renamed decimal dictionary
    JB

--- src/RomanNumberConverter.cs
+++ src/RomanNumberConverter.cs
@@ -5,7 +5,7 @@ namespace CraftingCode
 {
     public class RomanNumeralConverter
     {
-        private static Dictionary<char, int> _characterValues = new Dictionary<char, int>{
+        private static Dictionary<char, int> _decimalEquivalents = new Dictionary<char, int>{
             {'I', 1},
             {'V', 5},
             {'X', 10},
@@ -13,6 +13,16 @@ namespace CraftingCode
             {'C', 100},
             {'D', 500},
             {'M', 1000}
+        };
+
+         private static Dictionary<int, char> _romanEquivalents = new Dictionary<int, char>{
+            { 1, 'I'},
+            { 5, 'V'},
+            { 10, 'X'},
+            { 50, 'L'},
+            { 100, 'C'},
+            { 500, 'D'},
+            { 1000, 'M'}
         };
 
         public static string ConvertToRomanNumerals(int decimalNumber)
@@ -49,8 +59,8 @@ namespace CraftingCode
         private static bool IsValidDeductionTriple(Nullable<char> previousCharacter, char currentCharacter, char nextCharacter)
         {
 
-            return (!previousCharacter.HasValue || _characterValues[previousCharacter.Value] >=
-                   _characterValues[nextCharacter]) && _allowedDeductionPairs.ContainsKey(currentCharacter) &&
+            return (!previousCharacter.HasValue || _decimalEquivalents[previousCharacter.Value] >=
+                   _decimalEquivalents[nextCharacter]) && _allowedDeductionPairs.ContainsKey(currentCharacter) &&
                _allowedDeductionPairs[currentCharacter].Contains(nextCharacter);
         }
         public static int ConvertToDecimal(string romanNumeral)
@@ -58,10 +68,10 @@ namespace CraftingCode
             var runningTotal = 0;
             for (int i = 0; i < romanNumeral.Length; i++)
             {
-                var currentCharacterValue = _characterValues[romanNumeral[i]];
+                var currentCharacterValue = _decimalEquivalents[romanNumeral[i]];
                 var nextCharacterValue = 0;
                 bool isDeductionPair = i < romanNumeral.Length - 1 &&
-                    currentCharacterValue < (nextCharacterValue = _characterValues[romanNumeral[i + 1]]);
+                    currentCharacterValue < (nextCharacterValue = _decimalEquivalents[romanNumeral[i + 1]]);
                 if (isDeductionPair)
                 {
                     var previousCharacter = i > 0 ? romanNumeral[i - 1] : (Nullable<char>)null;
